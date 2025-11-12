b3165006bf0869c0205ec6ed688f831c94f3d0d8
Letícia Queiroz <letciawanderleyletw@gmail.com>
2025-11-12 15:17:32 +0000

    implementação inicial paleta

--- src/paleta/paleta.cpp
+++ src/paleta/paleta.cpp
@@ -2,3 +2,24 @@
 
 Sequencia<int> seq;
 int n = seq.obterTamanho();
+
+Paleta::Paleta(const char *arquivo) {
+    std::ifstream paleta(arquivo);
+    if (paleta.is_open()) {
+        while (!paleta.eof()) {
+            this->adicionarCor(); // fazer
+        }
+    }
+}
+
+void Paleta::adicionarCor(Cor cor) {
+    this->cores.adicionar(cor);
+}
+
+int Paleta::obterTamanho() {
+    return this->cores.obterTamanho();
+}
+
+Cor Paleta::obterCor(int indice) {
+    return this->cores[indice];
+}
--- src/paleta/paleta.h
+++ src/paleta/paleta.h
@@ -1,3 +1,21 @@
 #pragma once
 
 #include "../sequencia/sequencia.h"
+#include <iostream>
+#include <fstream>
+
+struct Cor {
+    uint8_t r, g, b;
+};
+
+class Paleta {
+private:
+    Sequencia<Cor> cores;
+    
+public:
+    Paleta() : cores() {}; // cria paleta vazia
+    Paleta(const char* arquivo); // cria paleta de arquivo
+    void adicionarCor(Cor cor);
+    int obterTamanho();
+    Cor obterCor(int indice);
+};
