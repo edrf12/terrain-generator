fda52e5ba30c5f61ba5294512103051bfc209339
edrf12 <me@edrf.party>
2025-12-09 19:20:33 -0300

    feat: Add new arguments to configure terrain generation

--- src/main.cpp
+++ src/main.cpp
@@ -3,49 +3,72 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <unistd.h>
 #include "paleta/paleta.h"
 #include "terreno/terreno.h"
 
 int main(int argc, char* argv[]) {
-  // Check if all arguments were given
-  if (argc != 4) {
-    std::cerr << "usage: " << argv[0] << " <paleta> <tamanho> <saida>" << std::endl;
+  int rugosidade = 2;
+  int tipo_imagem = IMAGEM_TIPO_LUZ;
+  int reducao = 2;
+  int opt;
+
+  while ((opt = getopt(argc, argv, "r:t:s:")) != -1) {
+    switch (opt) {
+      case 'r':
+        rugosidade = std::stoi(optarg);
+        break;
+      case 't':
+        tipo_imagem = std::stoi(optarg);
+        break;
+      case 's':
+        reducao = std::stoi(optarg);
+        break;
+      default: 
+        std::cerr << "usage: " << argv[0] << " [-r rugosidade] [-t 0 (grayscale) 3 (cores) 2 (sombreada)] [-s intensidade sombra] <paleta> <tamanho> <saida>" << std::endl;
+        return 1;
+    }
+  }
+
+  if (argc - optind != 3) {
+    std::cerr << "usage: " << argv[0] << " [-r rugosidade] [-t 0 (grayscale) 3 (cores) 2 (sombreada)] [-s intensidade sombra] <paleta> <tamanho> <saida>" << std::endl;
     return 1;
   }
 
+  const char* arq_paleta = argv[optind];
+  const char* tamanho_str = argv[optind + 1];
+  const char* arq_saida = argv[optind + 2];
+
   // Check if paleta is a valid file
-  std::ifstream paleta(argv[1]);
-
+  std::ifstream paleta(arq_paleta);
   if (!paleta.is_open()) {
-    std::cerr << argv[0] << ": " << argv[1] << ": File not found" << std::endl;
+    std::cerr << argv[0] << ": " << arq_paleta << ": File not found" << std::endl;
+    return 1;
   }
-
   paleta.close();
 
   // Check if tamanho is an integer
-  if (argv[2] == NULL || *argv[2] == '\0') {
-    std::cerr << argv[0] << ": Size cannot be null" << std::endl;
-    return 1;
-  }
-
-  for (int i = 0; argv[2][i] != '\0'; i++) {
-    if (!std::isdigit(argv[2][i])) {
+  for (int i = 0; tamanho_str[i] != '\0'; i++) {
+    if (!std::isdigit(tamanho_str[i])) {
       std::cerr << argv[0] << ": Size needs to be a number" << std::endl;
       return 1;
     }
   }
 
   // Check if we will not overwrite a existing file
-  std::ifstream saida(argv[3]);
-
+  std::ifstream saida(arq_saida);
   if (saida.is_open()) {
-    std::cerr << argv[0] << ": " << argv[3] << ": File already exists" << std::endl;
+    std::cerr << argv[0] << ": " << arq_saida << ": File already exists" << std::endl;
+    return 1;
   }
-
   saida.close();
   
-  Paleta cores(argv[1]);
-  Terreno altitudes(std::stoi(argv[2]));
-  altitudes.gerarTerreno();
-  altitudes.salvarImagem(IMAGEM_TIPO_COR, argv[3], cores);
+  Paleta cores(arq_paleta);
+  Terreno altitudes(std::stoi(tamanho_str));
+  altitudes.gerarTerreno(rugosidade);
+  altitudes.salvarImagem(tipo_imagem, arq_saida, cores, reducao);
+
+  std::cout << "Imagem '" << arq_saida << "' gerada com sucesso!" << std::endl;
+  
+  return 0;
 }
